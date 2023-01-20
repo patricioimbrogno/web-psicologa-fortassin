43fe5b0583892c99559ef08c0c3e3a373f991960
patricioimbrogno <patricioimbrogno@gmail.com>
2023-01-20 15:14:26 -0300

    blockquoute

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -21,15 +21,30 @@ const About = () => {
         item
         xs={12}
         sm={6}
-        sx={{ mt: "5%"}}
+        sx={{ mt: "5%" }}
         container
         justifyContent="center"
       >
-        <img src={ProfileImage} alt='Fiorella Fortassin' className="profileImage"></img>
+        <img
+          src={ProfileImage}
+          alt="Fiorella Fortassin"
+          className="profileImage"
+        ></img>
       </Grid>
 
-      <Grid item xs={12} sm={6} sx={{ mt: "5%", borderLeft: '8px solid #f9ac85' }} className="bio">
-        <Grid container display='flex' flexDirection='column' alignItems='center'>
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        sx={{ mt: "5%", borderLeft: "8px solid #f9ac85" }}
+        className="bio"
+      >
+        <Grid
+          container
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
           <Typography
             sx={{ color: "#545454" }}
             align="center"
@@ -38,28 +53,30 @@ const About = () => {
           >
             Licenciada en psicología, egresada de la Universidad Católica
             Argentina. Orientación de trabajo TCC (terapia cognitiva
-            conductual), especialmente en los trastornos del
-            neurodesarrollo como el autismo, TDAH y trastornos del lenguaje.
+            conductual), especialmente en los trastornos del neurodesarrollo
+            como el autismo, TDAH y trastornos del lenguaje.
           </Typography>
-
+          <span className={`content ${toggledClass} blockquoteMobile`}></span>
           <Typography
             sx={{ color: "#545454" }}
             align="center"
             variant="subtitle2"
             className={`content ${toggledClass} otro-blockquote`}
           >
-            <br />Desde hace 10 años me formo en el área de atención a personas con
+            <br />
+            Desde hace 10 años me formo en el área de atención a personas con
             discapacidad. Realicé varias disertaciones en congresos
             latinoamericanos y clases abiertas a la comunidad, cómo también
-            capacitaciones a centros educativos terapéuticos. <br /><br />Acompaño a los
-            niños y sus familias a transitar estos desafíos en el desarrollo,
-            con la mirada puesta en la calidad de vida de todo el sistema
-            familiar. Asimismo realizo evaluaciones en niños con sospecha de
-            autismo mediante test estandarizados cómo Ados-2, Adi-R y ASQ-3.
-            Llevo adelante además tratamientos basados en ABA, TEACCH, PECS y
-            Social Thinking.
+            capacitaciones a centros educativos terapéuticos. <br />
+            <br />
+            Acompaño a los niños y sus familias a transitar estos desafíos en el
+            desarrollo, con la mirada puesta en la calidad de vida de todo el
+            sistema familiar. Asimismo realizo evaluaciones en niños con
+            sospecha de autismo mediante test estandarizados cómo Ados-2, Adi-R
+            y ASQ-3. Llevo adelante además tratamientos basados en ABA, TEACCH,
+            PECS y Social Thinking.
           </Typography>
-
+          <span className={`content ${toggledClass} nuevo-blockquote`}></span>
           <br />
           <Button
             sx={{
@@ -74,7 +91,7 @@ const About = () => {
               },
             }}
             onClick={() => setExpanded(!expanded)}
-            className='hide'
+            className="hide"
           >
             {expanded ? "Leer menos" : "Leer más"}
           </Button>
